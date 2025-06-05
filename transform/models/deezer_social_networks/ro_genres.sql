905a303bf6c31f80ccce50e0109e7c3b314b4b99
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-06-05 18:16:13 +0100

    fix: wrong filename case, should be RO, not ro

--- transform/models/deezer_social_networks/ro_genres.sql
+++ transform/models/deezer_social_networks/ro_genres.sql
@@ -1,4 +1,4 @@
 {{ config(materialized='table') }}
 
 select *
-from read_json_auto('s3://lakehouse/raw/deezer_social_networks/2025_06_04/16_44_30_382/RO/ro_genres.json')
+from read_json_auto('s3://lakehouse/raw/deezer_social_networks/2025_06_04/16_44_30_382/RO/RO_genres.json')
