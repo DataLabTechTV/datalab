78bae87dbab67470f93d4db0d3a5655169a02236
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-06-06 18:17:11 +0100

    fix: sqlite prefix missing

--- transform/macros/attach_ducklake.sql
+++ transform/macros/attach_ducklake.sql
@@ -1,6 +1,6 @@
 {% macro attach_ducklake() %}
 
-ATTACH 'ducklake:{{ env_var('LOCAL_DIR') }}/{{ env_var('STAGE_DB') }}' AS stage
+ATTACH 'ducklake:sqlite:{{ env_var('LOCAL_DIR') }}/{{ env_var('STAGE_DB') }}' AS stage
 (DATA_PATH 's3://{{ env_var('S3_BUCKET') }}/{{ env_var('S3_STAGE_PREFIX') }}');
 
 {% endmacro %}
