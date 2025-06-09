701fb1d832eb52aedae62394ded25745c7cd602a
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-06-09 18:07:21 +0100

    fix: wrong reference, missing schema prefix

--- transform/models/marts/graphs/music_taste/nodes/msdsl_users.sql
+++ transform/models/marts/graphs/music_taste/nodes/msdsl_users.sql
@@ -2,5 +2,5 @@ SELECT DISTINCT
     'm_' || user_id AS node_id,
     user_id,
     'MSDSL' AS source
-FROM {{ ref('user_listening_history') }}
+FROM {{ ref('msdsl_user_listening_history') }}
 ORDER BY user_id
