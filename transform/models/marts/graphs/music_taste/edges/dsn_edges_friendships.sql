bfecf9f249186d4e6dcf1bc8f1ba65b82ea47606
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-06-11 10:53:12 +0100

    fix: missing node ID dataset-based prefix

--- transform/models/marts/graphs/music_taste/edges/dsn_edges_friendships.sql
+++ transform/models/marts/graphs/music_taste/edges/dsn_edges_friendships.sql
@@ -1,23 +1,20 @@
 SELECT
-    e.source_id AS source_id,
-    e.target_id AS target_id,
-    'HR' AS country
+    'd_hr' || e.source_id AS source_id,
+    'd_hr' || e.target_id AS target_id,
 FROM {{ ref('dsn_hr_edges') }} AS e
 
 UNION
 
 SELECT
-    e.source_id AS source_id,
-    e.target_id AS target_id,
-    'HU' AS country
+    'd_hu' || e.source_id AS source_id,
+    'd_hu' || e.target_id AS target_id,
 FROM {{ ref('dsn_hu_edges') }} AS e
 
 UNION
 
 SELECT
-    e.source_id AS source_id,
-    e.target_id AS target_id,
-    'RO' AS country
+    'd_ro' || e.source_id AS source_id,
+    'd_ro' || e.target_id AS target_id,
 FROM {{ ref('dsn_ro_edges') }} AS e
 
-ORDER BY e.source_id, e.target_id
+ORDER BY source_id, target_id
--- transform/models/marts/graphs/music_taste/edges/dsn_edges_user_genres.sql
+++ transform/models/marts/graphs/music_taste/edges/dsn_edges_user_genres.sql
@@ -1,18 +1,18 @@
 SELECT
-    'd_' || user_id AS source_id,
+    'd_hr_' || user_id AS source_id,
     unnest(genres) AS target_id
 FROM {{ ref('dsn_hr_genres' )}}
 
 UNION
 
 SELECT
-    'd_' || user_id AS source_id,
+    'd_hu_' || user_id AS source_id,
     unnest(genres) AS target_id
 FROM {{ ref('dsn_hu_genres' )}}
 
 UNION
 
 SELECT
-    'd_' || user_id AS source_id,
+    'd_hu_' || user_id AS source_id,
     unnest(genres) AS target_id
 FROM {{ ref('dsn_ro_genres' )}}
