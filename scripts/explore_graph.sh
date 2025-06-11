4562ab65721efdc48391d2a54ecdf4a7fb792a8b
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-06-11 19:11:17 +0100

    feat: quality of life for explorer startup and exit

--- scripts/explore_graph.sh
+++ scripts/explore_graph.sh
@@ -12,4 +12,40 @@ fi
 
 kuzudb_path=$1
 
-docker run -p 8000:8000 --rm -v "${kuzudb_path}:/database" kuzudb/explorer:latest
+if [[ "$kuzudb_path" != /* && "$kuzudb_path" != ./* ]]; then
+    kuzudb_path="./$kuzudb_path"
+fi
+
+cleanup() {
+    echo "==> Cleaning up kuzudb-explorer..."
+    trap - SIGINT SIGTERM
+
+    echo "Stopping kuzudb-explorer"
+    docker stop kuzudb-explorer >/dev/null
+
+    echo "Removing kuzudb-explorer"
+    docker rm kuzudb-explorer >/dev/null
+
+    if [ -e "$kuzudb_path/.lock" ]; then
+        echo "Removing database lock"
+        rm -f "$kuzudb_path/.lock"
+    fi
+}
+
+trap cleanup SIGINT SIGTERM
+
+if [ -e "$kuzudb_path/.lock" ]; then
+    echo "error: database is locked"
+    exit 3
+fi
+
+echo "==> Starting kuzudb-explorer..."
+docker run -q -d --name kuzudb-explorer -p 8000:8000 \
+    -v "${kuzudb_path}:/database" kuzudb/explorer:latest \
+    >/dev/null
+
+echo "==> Opening browser..."
+open http://localhost:8000
+
+echo "==> Following logs..."
+docker logs -f kuzudb-explorer
