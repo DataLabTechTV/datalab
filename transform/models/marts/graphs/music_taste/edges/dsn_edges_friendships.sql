3a0d6d97544f99c8b2d3d62e9a734b7490a0ced7
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-06-11 10:59:28 +0100

    fix: missing underscore after prefix

--- transform/models/marts/graphs/music_taste/edges/dsn_edges_friendships.sql
+++ transform/models/marts/graphs/music_taste/edges/dsn_edges_friendships.sql
@@ -1,20 +1,20 @@
 SELECT
-    'd_hr' || e.source_id AS source_id,
-    'd_hr' || e.target_id AS target_id,
+    'd_hr_' || e.source_id AS source_id,
+    'd_hr_' || e.target_id AS target_id,
 FROM {{ ref('dsn_hr_edges') }} AS e
 
 UNION
 
 SELECT
-    'd_hu' || e.source_id AS source_id,
-    'd_hu' || e.target_id AS target_id,
+    'd_hu_' || e.source_id AS source_id,
+    'd_hu_' || e.target_id AS target_id,
 FROM {{ ref('dsn_hu_edges') }} AS e
 
 UNION
 
 SELECT
-    'd_ro' || e.source_id AS source_id,
-    'd_ro' || e.target_id AS target_id,
+    'd_ro_' || e.source_id AS source_id,
+    'd_ro_' || e.target_id AS target_id,
 FROM {{ ref('dsn_ro_edges') }} AS e
 
 ORDER BY source_id, target_id
