dbfefa81f9ccd9421f6e8cd5fc01fb6dac2d2b8a
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-06-13 09:50:16 +0100

    feat: add CLI args for read only and to reset
    Container is now kept between sessions, unless explicitly reset.

--- scripts/explore_graph.sh
+++ scripts/explore_graph.sh
@@ -6,9 +6,57 @@ if ! which docker >/dev/null; then
 fi
 
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 KUZUDB_PATH"
+    echo "Usage: $0 [--reset] [--read-only] KUZUDB_PATH"
     exit 2
 fi
+
+cleanup() {
+    trap - SIGINT SIGTERM
+    echo "==> Stopping docker container for kuzudb-explorer"
+    docker stop kuzudb-explorer >/dev/null
+}
+
+container_exists() {
+    docker ps -a --format '{{.Names}}' |
+        awk '$0 == "kuzudb-explorer" { found=1 } END { exit !found }' >/dev/null
+}
+
+kuzudb_container_mode() {
+    mode=$(docker inspect kuzudb-explorer \
+        --format='{{range .Config.Env}}{{println .}}{{end}}' |
+        awk '{ if (match($0, /MODE=(.*)/, m)) print m[1] }')
+
+    if [ -z "$mode" ]; then
+        # default
+        mode=READ_WRITE
+    fi
+
+    echo "$mode"
+}
+
+trap cleanup SIGINT SIGTERM
+
+export MODE=READ_WRITE
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --reset)
+        if container_exists; then
+            echo "==> Removing existing kuzudb-explorer container"
+            docker rm -f kuzudb-explorer >/dev/null
+        fi
+
+        shift
+        ;;
+    --read-only)
+        export MODE=READ_ONLY
+        shift
+        ;;
+    *)
+        break
+        ;;
+    esac
+done
 
 kuzudb_path=$1
 
@@ -16,21 +64,29 @@ if [[ "$kuzudb_path" != /* && "$kuzudb_path" != ./* ]]; then
     kuzudb_path="./$kuzudb_path"
 fi
 
-cleanup() {
-    trap - SIGINT SIGTERM
-    echo "==> Stopping ephemeral docker container for kuzudb-explorer"
-    docker stop kuzudb-explorer >/dev/null
-}
+if container_exists; then
+    current_mode=$(kuzudb_container_mode)
+    echo $current_mode
+    echo $MODE
 
-trap cleanup SIGINT SIGTERM
+    if [ "$current_mode" != $MODE ]; then
+        echo "==> Removing existing $current_mode kuzudb-explorer container"
+        docker rm -f kuzudb-explorer >/dev/null
+    fi
+fi
 
-echo "==> Starting ephemeral docker container for kuzudb-explorer..."
-docker run -d --name kuzudb-explorer \
-    -p 8000:8000 -v "${kuzudb_path}:/database" \
-    -e MODE=READ_ONLY \
-    --rm kuzudb/explorer:latest >/dev/null
+if container_exists; then
+    echo "==> Starting existing docker container for kuzudb-explorer..."
+    docker start kuzudb-explorer >/dev/null
+else
+    echo "==> Creating and starting docker container for kuzudb-explorer..."
+    docker run -d --name kuzudb-explorer \
+        -p 8000:8000 -v "${kuzudb_path}:/database" \
+        -e MODE=$MODE \
+        kuzudb/explorer:latest >/dev/null
+fi
 
 echo "==> Opening browser at http://localhost:8000..."
 open http://localhost:8000
 
-docker logs -f kuzudb-explorer
+docker logs -n 4 -f kuzudb-explorer
