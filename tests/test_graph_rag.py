f72422478f13505d5c9545dd1d3ec68f3346a79b
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-06-27 20:01:51 +0100

    test: invoke test for GraphRAG runnable

---
+++ tests/test_graph_rag.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pytest
+
+from graph.rag import GraphRAG
+from shared.settings import LOCAL_DIR, env
+
+PROMPTS = (
+    "If like metal artists like Metallica or Iron Maiden, but also listen to IDM, what other artists and genres could I listen to?",
+)
+
+
+def setup_module():
+    db_path = Path(LOCAL_DIR) / env.str("MUSIC_TASTE_GRAPH_DB")
+
+    if not db_path.exists():
+        pytest.skip(f"Database not found at {db_path}, skipping test.")
+
+
+def test_graph_rag():
+    gr = GraphRAG("music_taste")
+
+    for prompt in PROMPTS:
+        result = gr.invoke(dict(user_query=prompt))
+        print(result.content)
