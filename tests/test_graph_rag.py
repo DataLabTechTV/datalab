d2963e3b1027de909f64f497629004ac6a7514a3
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-07-02 12:18:02 +0100

    test: move graph db check to global fixtures

--- tests/test_graph_rag.py
+++ tests/test_graph_rag.py
@@ -1,24 +1,14 @@
-from pathlib import Path
-
-import pytest
+from fixtures import graph_db_schema
 
 from graph.rag import GraphRAG
-from shared.settings import LOCAL_DIR, env
 
 PROMPTS = (
     "If I like metal artists like Metallica or Iron Maiden, but also listen to IDM, what other artists and genres could I listen to?",
 )
 
 
-def setup_module():
-    db_path = Path(LOCAL_DIR) / env.str("MUSIC_TASTE_GRAPH_DB")
-
-    if not db_path.exists():
-        pytest.skip(f"Database not found at {db_path}, skipping test.")
-
-
-def test_graph_rag():
-    gr = GraphRAG("music_taste")
+def test_graph_rag(graph_db_schema):
+    gr = GraphRAG(graph_db_schema)
 
     for prompt in PROMPTS:
         response = gr.invoke(dict(user_query=prompt))
