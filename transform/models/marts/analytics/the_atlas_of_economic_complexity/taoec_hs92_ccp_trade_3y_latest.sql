85994988af3ae3c3283b3cba2e9247b83d020bf4
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-07-16 09:23:36 +0100

    feat: logic changed to account for the last 3 years in data instead of a fixed range

--- transform/models/marts/analytics/the_atlas_of_economic_complexity/taoec_hs92_ccp_trade_3y_latest.sql
+++ transform/models/marts/analytics/the_atlas_of_economic_complexity/taoec_hs92_ccp_trade_3y_latest.sql
@@ -1,4 +1,4 @@
-{{ config(alias='hs92_ccp_trade_2020_2023', materialized='view') }}
+{{ config(alias='hs92_ccp_trade_3y_latest', materialized='view') }}
 
 SELECT
     country_id,
@@ -7,9 +7,15 @@ SELECT
     partner_iso3_code,
     product_id,
     product_hs92_code,
+    min(year) AS since_year,
+    max(year) AS until_year,
     sum(export_value) AS export_value,
     sum(import_value) AS import_value
 FROM {{ ref('taoec_hs92_ccp_trade') }}
+WHERE year >= (
+    SELECT max(year) - 3
+    FROM stage.taoec.hs92_ccp_trade
+)
 GROUP BY
     country_id,
     country_iso3_code,
