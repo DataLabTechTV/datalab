0ca03462e6c1396bc207caec28bddb0584c6a225
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-07-16 11:10:58 +0100

    feat: country-country ESI calculation

---
+++ transform/models/marts/analytics/the_atlas_of_economic_complexity/taoec_cc_metrics.sql
@@ -0,0 +1,29 @@
+{{ config(alias='cc_metrics') }}
+
+WITH shares AS (
+    SELECT
+        country_id,
+        country_iso3_code,
+        partner_country_id,
+        partner_iso3_code,
+        product_id,
+        product_hs92_code,
+        export_value / sum(export_value) OVER (PARTITION BY country_id) AS share
+    FROM {{ ref('taoec_hs92_ccp_trade_3y_latest') }}
+)
+SELECT
+    a.country_id AS country_id_1,
+    a.country_iso3_code AS country_iso3_code_1,
+    b.country_id AS country_id_2,
+    b.country_iso3_code AS country_iso3_code_2,
+    sum(least(a.share, b.share)) AS esi
+FROM shares a
+JOIN shares b
+ON a.product_id = b.product_id
+    AND a.partner_country_id = b.partner_country_id
+WHERE a.country_id <> b.country_id
+GROUP BY
+    a.country_id,
+    a.country_iso3_code,
+    b.country_id,
+    b.country_iso3_code
