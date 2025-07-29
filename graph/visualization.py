3210fa585450707b323dcf5ce9d69a44cfe9c2f7
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-07-29 11:10:12 +0100

    feat: create a basic graph theme matching DLT

--- graph/visualization.py
+++ graph/visualization.py
@@ -1,5 +1,13 @@
+from typing import Optional
+
 import matplotlib.pyplot as plt
 import networkx as nx
+
+COLOR_PALETTE = [
+    "#42b0f9",
+    "#ffcc00",
+    "#ff5c92",
+]
 
 
 def plot(
@@ -7,6 +15,7 @@ def plot(
     label: str = "label",
     figsize: tuple[int, int] = (8, 5),
     margin: float = 0.2,
+    font_family: Optional[str] = None,
 ):
     _, ax = plt.subplots(figsize=figsize)
 
@@ -21,10 +30,31 @@ def plot(
         G,
         pos=pos,
         ax=ax,
-        node_color="skyblue",
+        node_color=COLOR_PALETTE[0],
         node_size=1000,
     )
-    nx.draw_networkx_edges(G, pos=pos, ax=ax)
-    nx.draw_networkx_labels(G, pos=pos, ax=ax, labels=labels)
+
+    nx.draw_networkx_edges(
+        G,
+        pos=pos,
+        ax=ax,
+        edge_color=COLOR_PALETTE[2],
+    )
+
+    nx.draw_networkx_labels(
+        G,
+        pos=pos,
+        ax=ax,
+        labels=labels,
+        font_family=font_family,
+        font_color=COLOR_PALETTE[2],
+        font_size=10,
+        bbox=dict(
+            facecolor="white",
+            edgecolor=COLOR_PALETTE[2],
+            boxstyle="round,pad=0.4",
+            alpha=0.85,
+        ),
+    )
 
     plt.show()
