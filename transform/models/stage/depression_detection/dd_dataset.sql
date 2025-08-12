11b448e8e980439006d365ae9336025775befa38
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-08-12 08:40:32 +0100

    fix: train/test split now separate from cross-validation (train only)

--- transform/models/stage/depression_detection/dd_dataset.sql
+++ transform/models/stage/depression_detection/dd_dataset.sql
@@ -18,18 +18,34 @@ WITH dataset AS (
             is_depression: BOOLEAN
         }
     )
+),
+train_test_split AS (
+    SELECT doc_id, text, label, (rn <= cnt * 0.2) AS is_test
+    FROM dataset
+),
+train_set AS (
+    SELECT
+        doc_id,
+        text,
+        label,
+        is_test,
+        row_number() OVER (PARTITION BY label ORDER BY text) AS rn,
+        count(*) OVER (PARTITION BY label) AS cnt
+    FROM train_test_split
+    WHERE NOT is_test
 )
 SELECT
+    doc_id,
     text,
     label,
-    (rn <= cnt * 0.2) AS is_test,
+    is_test,
     CASE
         WHEN rn < cnt * 0.2 THEN 0
         WHEN rn >= cnt * 0.2 AND rn < cnt * 0.4 THEN 1
         WHEN rn >= cnt * 0.4 AND rn < cnt * 0.6 THEN 2
         WHEN rn >= cnt * 0.6 AND rn < cnt * 0.8 THEN 3
         ELSE 4
-    END AS "folds_5_id",
+    END AS folds_5_id,
     CASE
         WHEN rn < cnt * 0.1 THEN 0
         WHEN rn >= cnt * 0.1 AND rn < cnt * 0.2 THEN 1
@@ -41,8 +57,13 @@ SELECT
         WHEN rn >= cnt * 0.7 AND rn < cnt * 0.8 THEN 7
         WHEN rn >= cnt * 0.8 AND rn < cnt * 0.9 THEN 8
         ELSE 9
-    END AS "folds_10_id"
-FROM
-    dataset
-ORDER BY
-    doc_id
+    END AS folds_10_id
+FROM train_set
+
+UNION
+
+SELECT doc_id, text, label, is_test, NULL AS folds_5_id, NULL AS folds_10_id
+FROM train_test_split
+WHERE is_test
+
+ORDER BY doc_id
