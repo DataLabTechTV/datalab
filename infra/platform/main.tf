09816d43240c3c704e3367e424fb8458c0f89f22
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-09-22 17:06:55 +0100

    fix: should be the node name, not the endpoint

--- infra/platform/main.tf
+++ infra/platform/main.tf
@@ -56,7 +56,7 @@ resource "random_password" "docker_vm" {
 resource "proxmox_virtual_environment_download_file" "ubuntu_24_04_qcow2" {
   content_type = "iso"
   datastore_id = "local"
-  node_name    = var.pm_endpoint
+  node_name    = var.pm_node
 
   url       = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
   file_name = "ubuntu-24.04-noble-server-cloudimg-amd64.img"
