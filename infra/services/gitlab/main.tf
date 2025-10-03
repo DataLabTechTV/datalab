c52f9401ea5caca1ed55eaea6ee3ce1426467d77
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-10-03 10:29:21 +0100

    fix: mark env var secrets as sensitive

--- infra/services/gitlab/main.tf
+++ infra/services/gitlab/main.tf
@@ -1,5 +1,5 @@
 locals {
-  secret_regex = "(?i)PASSWORD|ACCESS_KEY|SECRET"
+  secret_regex = "(?i)PASSWORD|SECRET"
 }
 
 data "dotenv" "config" {
@@ -7,10 +7,22 @@ data "dotenv" "config" {
 }
 
 resource "gitlab_project_variable" "datalab_config" {
-  for_each      = data.dotenv.config.env
+  for_each      = { for k, v in data.dotenv.config.env : k => v if !can(regex(local.secret_regex, k)) }
   project       = "${var.gitlab_user}/${var.gitlab_project}"
   key           = each.key
   value         = each.value
+  variable_type = "env_var"
+  protected     = false
+  raw           = true
+  hidden        = false
+  masked        = can(regex(local.secret_regex, each.key))
+}
+
+resource "gitlab_project_variable" "datalab_secrets" {
+  for_each      = { for k, v in data.dotenv.config.env : k => v if can(regex(local.secret_regex, k)) }
+  project       = "${var.gitlab_user}/${var.gitlab_project}"
+  key           = each.key
+  value         = sensitive(each.value)
   variable_type = "env_var"
   protected     = false
   raw           = true
