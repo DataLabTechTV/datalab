9fff4c1b5b55569881ed443d707bc2f78cd6c54b
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-10-10 11:43:47 +0100

    refactor: make it clear that create database can safely fail

--- .ci/scripts/postgres/create_db.sh
+++ .ci/scripts/postgres/create_db.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 echo "Creating database: $DB_NAME"
-docker exec datalab-postgres-1 \
-    psql -c "CREATE DATABASE $DB_NAME"
+
+docker exec datalab-postgres-1
+    psql -c "CREATE DATABASE $DB_NAME" --set ON_ERROR_STOP=off
 
 echo "Granting all privileges to user: $DB_USER"
+
 docker exec datalab-postgres-1 \
-    psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+    psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER"
 
 echo DB_NAME="$DB_NAME" > credentials.env
 echo DB_USER="$DB_USER" >> credentials.env
