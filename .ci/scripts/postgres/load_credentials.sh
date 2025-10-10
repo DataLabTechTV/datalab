2642e034ac66559f94d88f2a5da81230ab6a8945
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-10-10 09:44:39 +0100

    fix: set env file to the ci project dir

--- .ci/scripts/postgres/load_credentials.sh
+++ .ci/scripts/postgres/load_credentials.sh
@@ -3,5 +3,5 @@
 PSQL_SECRETS=${PSQL_SECRETS:-"{}"}
 DB_PASS="$(echo "$PSQL_SECRETS" | jq -r '.["$[[ inputs.db_user ]]"]')"
 
-echo DB_USER="$DB_USER" > credentials.env
-echo DB_PASS="$DB_PASS" >> credentials.env
+echo DB_USER="$DB_USER" > "$CI_PROJECT_DIR/credentials.env"
+echo DB_PASS="$DB_PASS" >> "$CI_PROJECT_DIR/credentials.env"
